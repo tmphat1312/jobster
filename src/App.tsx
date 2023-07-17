ba44d9382ed735c51918c44470077495db971111
tmphat1312 <tmpsummer@gmail.com>
2023-07-17 13:21:56 +0700

    initiate jobster project repo structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import logo from "@images/logo.svg"
+import main from "@images/main.svg"
+
+function App() {
+  return (
+    <div>
+      <img src={logo} alt="" />
+      <img src={main} alt="" />
+      <p className="text-primary-900">I'm typing some text.</p>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-50": "#eff6ff",
+        "primary-100": "#dbeafe",
+        "primary-200": "#bfdbfe",
+        "primary-300": "#93c5fd",
+        "primary-400": "#60a5fa",
+        "primary-500": "#3b82f6",
+        "primary-600": "#2563eb",
+        "primary-700": "#1d4ed8",
+        "primary-800": "#1e40af",
+        "primary-900": "#1e3a8a",
+      },
+    },
+  },
+  plugins: [],
+}
