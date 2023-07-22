3fc7105d2025cbbdd89d85917a418ed5941d651e
tmphat1312 <tmpsummer@gmail.com>
2023-07-22 08:43:54 +0700

    refactored: custom primary colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        "primary-50": "#eff6ff",
-        "primary-100": "#dbeafe",
-        "primary-200": "#bfdbfe",
-        "primary-300": "#93c5fd",
-        "primary-400": "#60a5fa",
-        "primary-500": "#3b82f6",
-        "primary-600": "#2563eb",
-        "primary-700": "#1d4ed8",
-        "primary-800": "#1e40af",
-        "primary-900": "#1e3a8a",
+        primary: {
+          50: "#eff6ff",
+          100: "#dbeafe",
+          200: "#bfdbfe",
+          300: "#93c5fd",
+          400: "#60a5fa",
+          500: "#3b82f6",
+          600: "#2563eb",
+          700: "#1d4ed8",
+          800: "#1e40af",
+          900: "#1e3a8a",
+        },
         pending: {
           text: colors.orange[500],
           bg: colors.orange[200],
